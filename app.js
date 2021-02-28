feeea2c0d01c78c8a1f99c5d1f8fee44b1fbeaa8
Alejandra Moreno Vallejo <alejamorenovallejo@gmail.com>
2021-02-28 00:31:33 -0500

    Ajuste de Menú

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-$('.nav').on('click', 'li', function() {
-    $(this).toggleClass('active');
+var count=0;
+$('.nav').on('click','li', function() {
+    //Se agrega al elemento li un atributo id con un valor autoincrementable
+    $(this).attr("id",count++);
+    //Se toma el id anterior que tiene la clase active removerla y asignarla al nuevo elemento
+    idAnt=this.id - 1
+    $('#'+idAnt).removeClass('active');
+    //Al id anterior se le asigna el color negro en el elemento de link(a)
+    $('#'+idAnt).children('a').css('color','#000');
+    //Al nuevo elemento se le agrega la clase 'active'
+    $(this).addClass('active');   
 });
 
-$('.nav').on('mouseover', 'li', function() {
-    $(this).toggleClass('active');
+$('.nav li a').on('mouseover', function() {
+    $(this).css('color','#EA178C');    
 });
 
-$('.nav').on('mouseleave', 'li', function() {
-    $(this).toggleClass('active');
+$('.nav').on('mouseleave','li', function() {
+    if($(this).attr('class') !='active') {
+        $(this).children('a').css('color','#000');
+    }
 });
+
+
+
+
+
+
