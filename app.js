2b09763153513cdca7246879358a658786d0d857
Alejandra Moreno Vallejo <alejamorenovallejo@gmail.com>
2021-03-02 19:11:41 -0500

    Ajuste Menu

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var count=0;
 $('.nav').on('click','li', function() {
     //Se agrega al elemento li un atributo id con un valor autoincrementable
     $(this).attr("id",count++);
-    //Se toma el id anterior que tiene la clase active removerla y asignarla al nuevo elemento
-    idAnt=this.id - 1
+    //Se toma el id anterior que tiene la clase active para removerla y asignarla al nuevo elemento.
+    idAnt=this.id-1
     $('#'+idAnt).removeClass('active');
-    //Al id anterior se le asigna el color negro en el elemento de link(a)
+    //Se cambia el elemento anterior a color negro.
     $('#'+idAnt).children('a').css('color','#000');
-    //Al nuevo elemento se le agrega la clase 'active'
+    //Se agrega la nuevo elemento seleccionado la clase 'active'
     $(this).addClass('active');   
 });
 
@@ -16,6 +16,7 @@ $('.nav li a').on('mouseover', function() {
 });
 
 $('.nav').on('mouseleave','li', function() {
+    //Valida que si la clase es diferente a 'active' se cambia a color negro
     if($(this).attr('class') !='active') {
         $(this).children('a').css('color','#000');
     }
